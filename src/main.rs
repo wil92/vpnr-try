aabe095968651153d8da0b46361ec7e72f6899cd
Guillermo <ggjnez92@gmail.com>
2023-06-02 02:04:05 +0200

    Update communication between server and client

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::thread;
 
 pub mod tcp;
 
@@ -24,40 +23,35 @@ fn main() {
 fn start_server() {
     println!("Server start");
 
-
     tcp::tcp_server(
         4334,
-        move |stream: &mut TcpStream, client: &mut TcpStream| {
+        move |client: &mut TcpStream| loop {
             let mut buf = [0; 10];
-            stream.read(&mut buf).unwrap();
-
+            client.read(&mut buf).unwrap();
             client.write(&buf).unwrap();
-            println!("read: {:?}", buf);
         },
-        |_, _| {},
+        move |_| {},
     );
 }
 
 fn start_client() {
     println!("Client start");
 
-    let mut to_server_stream = TcpStream::connect("127.0.0.1:4334").unwrap();
-
-    tcp::tcp_server(
+    tcp::tcp_client(
         4333,
-        &mut to_server_stream,
-        move |stream: &mut TcpStream, server: &mut TcpStream| {
+        4334,
+        move |stream: &mut TcpStream, server: &mut TcpStream| loop {
             let mut buf = [0; 10];
             stream.read(&mut buf).unwrap();
 
             server.write(&buf).unwrap();
             println!("read: {:?}", buf);
         },
-        move |stream: &mut TcpStream, server: &mut TcpStream| {
+        move |stream: &mut TcpStream, server: &mut TcpStream| loop {
             let mut buf = [0; 10];
             server.read(&mut buf).unwrap();
 
-            stream.write(&buf);
+            stream.write(&buf).unwrap();
         },
     );
 }
--- src/tcp.rs
+++ src/tcp.rs
@@ -3,17 +3,52 @@ use std::thread;
 
 pub mod protocol;
 
-pub fn tcp_server(port: i32, server: &mut TcpStream, f: fn(&mut TcpStream, &mut TcpStream), f2: fn(&mut TcpStream, &mut TcpStream)) {
+pub fn tcp_client(
+    client_port: i32,
+    server_port: i32,
+    f: fn(&mut TcpStream, &mut TcpStream),
+    f2: fn(&mut TcpStream, &mut TcpStream),
+) {
+    let listener = TcpListener::bind(format!("127.0.0.1:{client_port}")).unwrap();
+
+    let to_server_stream = TcpStream::connect(format!("127.0.0.1:{server_port}")).unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let mut stream_read = stream.try_clone().expect("Error clonning");
+                let mut stream_write = stream.try_clone().expect("Error clonning");
+                let mut server_read = to_server_stream.try_clone().expect("Error clonning");
+                let mut server_write = to_server_stream.try_clone().expect("Error clonning");
+                thread::spawn(move || {
+                    f(&mut stream_read, &mut server_write);
+                });
+                thread::spawn(move || {
+                    f2(&mut stream_write, &mut server_read);
+                });
+            }
+            Err(_) => println!("couldn't get client: "),
+        }
+    }
+}
+
+pub fn tcp_server(
+    port: i32,
+    f: fn(&mut TcpStream),
+    f2: fn(&mut TcpStream),
+) {
     let listener = TcpListener::bind(format!("127.0.0.1:{port}")).unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
+                let mut stream_read = stream.try_clone().expect("Error clonning");
+                let mut stream_write = stream.try_clone().expect("Error clonning");
                 thread::spawn(move || {
-                    f(&mut stream, server);
+                    f(&mut stream_read);
                 });
                 thread::spawn(move || {
-                    f2(&mut stream, server);
+                    f2(&mut stream_write);
                 });
             }
             Err(_) => println!("couldn't get client: "),
